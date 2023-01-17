b24fadd1dde386ad80fee0effb4898b7e05313d5
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2023-01-16 22:23:53 -0300

    Acessando path do OS

--- hello.py
+++ hello.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
 # Cometario Python
+"""
 
+Utiliznado variavel de ambiente
 
-print('Hello, Word!')
+Como usar:
+tenha a variavel LANG configurada
 
-print('lucas'.upper())
-print(2+3)
+    export=pt_BR
+
+"""
+__version__ = "0.0.1"
+__author__ = "Lucas FAvaretto"
+__license__ = "Unlicense"
+
+import os
+
+current_languge = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, Word!"
+
+if current_languge == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_languge == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
 
