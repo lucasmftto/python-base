d7f67ffc72aae69871ef6f88f97c6bdb3ef153c9
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2023-03-12 16:39:46 -0300

    Trabalhando com dicionario

--- hello.py
+++ hello.py
@@ -11,19 +11,22 @@ tenha a variavel LANG configurada
     export=pt_BR
 
 """
-__version__ = "0.0.1"
+__version__ = "0.0.2    "
 __author__ = "Lucas FAvaretto"
 __license__ = "Unlicense"
 
 import os
 
 current_languge = os.getenv("LANG", "en_US")[:5]
-msg = "Hello, Word!"
 
-if current_languge == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_languge == "it_IT":
-    msg = "Ciao, Mondo!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo",
+    "es_SP": "Holam Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
 
-print(msg)
+
+print(msg[current_languge])
 
